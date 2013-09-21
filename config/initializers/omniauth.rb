acfd53ab18b3e865218518cdc09cf758c24f18b3
Jonathan Ng <jonathanng91@gmail.com>
2013-09-21 19:12:19 -0400

    Forces local environment for FB API ID and Secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '541365185935224', '7fc10157712fe6fce4afa8e48925aa62'
+  provider :facebook, ENV['FB_API_ID'], ENV['FB_API_SECRET']
 end
