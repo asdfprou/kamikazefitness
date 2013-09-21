38816456a96d19d6ee2bc8072a654c97e4dac8a6
Jonathan Ng <jonathanng91@gmail.com>
2013-09-21 19:32:40 -0400

    Hardcodes FB App ID into the js file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FB_API_ID'], ENV['FB_API_SECRET']
+  provider :facebook, ENV["FB_API_ID"], ENV["FB_API_SECRET"]
 end
